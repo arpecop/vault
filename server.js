bec1abfb8d789ece670f08548316e26cf2dbc32e
Radoslav <rudix.lab@gmail.com>
2021-01-03 14:54:01 +0200

    c

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 3000;
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-app.get("/", (req, res) => {
-  res.send("Hello World1!");
-});
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const express = require("express");
+  const app = express();
+  app.get("/", (req, res) => {
+    res.send("Hello World1!");
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
